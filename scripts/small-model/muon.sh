48e8d5cd258e7da4ccd4e28f7135be48184fff77
Matthew W Shumway <mattwshumway7@gmail.com>
2025-11-30 18:30:37 -0500

    sized folders for scripts

---
+++ scripts/small-model/muon.sh
@@ -0,0 +1,22 @@
+
+python3.10 train.py config/train_shakespeare_char.py \
+--optimizer_variant=muon \
+--device='mps' \
+--compile=False \
+--eval_iters=20 \
+--log_interval=1 \
+--block_size=32 \
+--batch_size=32 \
+--n_layer=4 \
+--n_head=4 \
+--n_embd=32 \
+--max_iters=2000 \
+--lr_decay_iters=2000 \
+--dropout=0.0 \
+--wandb_log=True \
+--wandb_group_name='muon' \
+--wandb_run_name='muon-lr0.01' \
+--lr_finder=False \
+--learning_rate=0.01 \
+
+
